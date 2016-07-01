9e443de61723cd9c40cf4b7394835a90c3959d55
adhoc <adhoc@adhocs-imac.local>
2016-07-01 15:47:46 -0700

    Initial Commit

---
+++ taxes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taxes
+//
+//  Created by adhoc on 26/09/1437 AH.
+//  Copyright © 1437 AH adhoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
